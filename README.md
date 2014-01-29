7046ea81a0fdc7e5af105c5a9e06123cb0b52fc4
David Cornu <davidjcornu@gmail.com>
2014-01-29 16:21:24 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Slow Image
+
+Serves an image really slowly to test timeout settings.
+
+## Usage
+
+```shell
+$ git clone git@github.com:davidcornu/slow_image.git
+$ cd slow_image
+$ node app.js
+```
---
+++ app.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var http = require('http');
+var fs   = require('fs');
+var path = require('path');
+
+var port      = process.env.PORT || 3000;
+var imgPath   = path.resolve(__dirname, './image.jpg');
+var chunkSize = 10240; // 10KB
+var waitTime  = 100; // ms
+
+// Load the image into memory once 
+var loadImgBuf = (function(){
+  var imgBuf;
+
+  return function(callback){
+    if (imgBuf) return process.nextTick(function(){ callback(null, imgBuf) });
+
+    fs.readFile(imgPath, function(err, buffer){
+      if (err) return callback(err);
+      imgBuf = buffer;
+      callback(null, imgBuf);
+    });
+  };
+})();
+
+var server = http.createServer(function(req, res){
+  loadImgBuf(function(err, imgBuf){
+    if (err) {
+      res.statusCode = 500;
+      res.setHeader('content-type', 'text/plain');
+      return res.end('An error occured');
+    }
+
+    res.statusCode = 200;
+    res.setHeader('content-type', 'image/jpeg');
+    
+    var imgLen = imgBuf.length;
+    var curPos = 0;
+
+    function feed(){
+      if (curPos >= imgLen) return res.end();
+
+      var nextPos = curPos + chunkSize;
+      if (nextPos > imgLen) nextPos = imgLen;
+      res.write(imgBuf.slice(curPos, nextPos));
+      curPos = nextPos;
+      
+      setTimeout(feed, waitTime);
+    }
+
+    feed();
+  });
+});
+
+server.listen(port, function(){
+  console.log('Server listening on port %s', port);
+});
